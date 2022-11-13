3ff20bc5b8a9e020f6f686a43f70e94963b49f56
Meshkat Ahammed <meshkatuddinahammed@outlook.com>
2022-11-13 18:28:12 +0600

    Created Models

--- learningapp/admin.py
+++ learningapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Quiz,DiscussionForum,Article
 # Register your models here.
+admin.site.register(Quiz)
+admin.site.register(DiscussionForum)
+admin.site.register(Article)
--- learningapp/models.py
+++ learningapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Article(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='article_author')
+    title = models.CharField(max_length=264,verbose_name='Put a title')
+    slug = models.SlugField(max_length=264,unique=True)
+    content = models.TextField(verbose_name='Write your mind')
+    image = models.ImageField(upload_to='media/images/articleimages',blank=True)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish_date',]
+    def __str__(self):
+        return self.title
+
+class Quiz(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='user_quiz')
+    question = models.CharField(max_length=264,verbose_name='Write the question')
+    option1 = models.CharField(max_length=264,null=True)
+    option2 = models.CharField(max_length=264,null=True)
+    option3 = models.CharField(max_length=264,null=True)
+    option4 = models.CharField(max_length=264,null=True)
+    answer = models.CharField(max_length=264,null=True)
+
+    def __str__(self):
+        return self.question
+
+class DiscussionForum(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='user_question')
+    question = models.CharField(max_length=264,verbose_name='Write the question')
+    answer = models.TextField(verbose_name='Write the answer',null=True)
+
+    def __str__(self):
+        return self.question
