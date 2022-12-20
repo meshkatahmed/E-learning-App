d25d531d4047a7bb910c33352171dc692d744db4
Meshkat Ahammed <meshkatuddinahammed@outlook.com>
2022-12-20 18:01:22 +0600

    Class-based view for adding article is created

--- learning_app/urls.py
+++ learning_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'learning_app'
 
 urlpatterns = [
-
+    path('',views.home,name='home'),
+    path('addarticle/',views.AddArticle.as_view(),name='addarticle'),
 ]
--- learning_app/views.py
+++ learning_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponseRedirect
+from django.http import HttpResponse
+from . import models
+from django.urls import reverse,reverse_lazy
+from django.views.generic import View,CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+import uuid
 # Create your views here.
+def home(request):
+    return HttpResponse('Welcome to homepage')
+class AddArticle(CreateView,LoginRequiredMixin):
+    model = models.Article
+    fields = ['title','content','image']
+    template_name = 'learning_app/addarticle.html'
+
+    def form_valid(self,form):
+        article_obj = form.save(commit=False)
+        article_obj.author = self.request.user
+        article_obj.slug = article_obj.title.replace(" ","-") + "-" + str(uuid.uuid4())
+        article_obj.save()
+        return HttpResponseRedirect(reverse('home'))
